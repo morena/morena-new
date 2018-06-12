b6b2fafc4a0d441a159c3c1da9d73c3f041f0ea8
Morena Fiore <morenafiore@gmail.com>
2018-06-12 22:54:08 +0100

    Resouces

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function printy($var){
+  echo '<pre>';
+  var_dump($var);
+  echo '</pre>';
+}
+
 // enqueue the child theme stylesheet
 
 Function wp_schools_enqueue_scripts() {
@@ -7,3 +13,13 @@ wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  
 wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+
+add_filter( 'woocommerce_product_tabs', 'remove_woocommerce_product_tabs', 98 );
+
+function remove_woocommerce_product_tabs( $tabs ) {
+    unset( $tabs['description'] );
+    return $tabs;
+}
+
+//add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_description_tab' );
